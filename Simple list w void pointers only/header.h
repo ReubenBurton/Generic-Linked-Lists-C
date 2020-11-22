6ff474e5a5a865e6d511c929477f3c2e9a9c06fe
ReubenBurton <ReubenBurton@users.noreply.github.com>
2020-11-22 22:11:55 +0800

    Add files via upload

---
+++ Simple list w void pointers only/header.h
@@ -0,0 +1,45 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include "newSleep.h"
+
+typedef struct data
+{
+    int hp;
+    int atk;
+    int def;
+    int med;
+    char* Name;
+}data;
+ 
+typedef struct Node
+{
+    void* data;
+    struct Node* next;
+}Node;
+ 
+typedef struct linkedList
+{
+    Node* head;
+}linkedList;
+ 
+ /*----------------------inclusions from linkedList.c------------*/
+ 
+linkedList* create_empty_list(void);
+Node* insert_end(linkedList* list, void* data);
+Node* insert_start(linkedList* list, void* data);
+Node* remove_start(linkedList* list);
+Node* remove_end(linkedList* list);
+Node* remove_specific(linkedList* list, int index);
+int length_of_list(linkedList* list);
+void free_list(linkedList* list);
+ 
+void printList(linkedList* list, void (*fptr)(void *));
+void printInt(void *i);
+void printFloat(void *f);
+void printString(void* s);
+void printEnemyStart(void* s);
+ 
+ 
+#endif
+
